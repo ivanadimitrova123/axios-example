4e50755809a441ca035841168942ba0b7f001bd6
ivanadimitrova123 <ivanadimitrova23@gmail.com>
2022-07-27 13:28:05 +0200

    aded axios call

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <template>
-  <Tutorial/>
+  <main>
+    <button @click="onButtonClick">Click me</button>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  async created() {
+    console.log(await this.$axios.$get('/api/v3/ticker/24hr'))
+  },
+  data(){
+    return{
+      error:'',
+    }
+  },
+  methods:{
+    async onButtonClick() {
+      this.error=''
+      try {
+        const data = await this.$axios.$get('/api/v3/ticker/24hr');
+        console.log(data);
+      } catch (e) {
+        this.error="There was an error"
+        console.log(e)
+      }
+    }
+  }
 }
 </script>
